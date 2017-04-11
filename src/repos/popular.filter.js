f0159aacfab2976bbad406284fa6cd8baa295261
Sara Basile <sarabasile@Saras-Air.lan>
2017-04-11 15:31:01 -0400

    added sort function to popular filter

--- src/repos/popular.filter.js
+++ src/repos/popular.filter.js
@@ -6,10 +6,24 @@
 
     function popular() {
 
-      return function popularity() {
+      return function sortByPopularity(repos) {
+        if(!Array.isArray(repos)) {
+          return repos;
+        }
+
+        return repos.sort(function sortRepos(a,b) {
+          return b - a;
+        });
 
       };
+
+      function popularity(a, b, c) {
+        return a + b*2 + c/2;
+      }
 
     }
 
 }());
+
+
+//stargazers_count + forks_count * 2  + open_issues_count / 2
